c8bd9c12dfcf41c400384cb0107b6d305dab2d49
Penoder <Rocking7189@163.com>
2017-09-03 10:52:40 +0800

    Initial commit

---
+++ refresh-layout/src/main/java/com/penoder/refreshlayout/listener/OnLoadmoreListener.java
@@ -0,0 +1,12 @@
+package com.penoder.refreshlayout.listener;
+
+import com.penoder.refreshlayout.api.RefreshLayout;
+
+/**
+ * 加载更多监听器
+ * Created by SCWANG on 2017/5/26.
+ */
+
+public interface OnLoadmoreListener {
+    void onLoadmore(RefreshLayout refreshlayout);
+}
---
+++ refresh-layout/src/main/java/com/penoder/refreshlayout/listener/OnRefreshListener.java
@@ -0,0 +1,12 @@
+package com.penoder.refreshlayout.listener;
+
+import com.penoder.refreshlayout.api.RefreshLayout;
+
+/**
+ * 刷新监听器
+ * Created by SCWANG on 2017/5/26.
+ */
+
+public interface OnRefreshListener {
+    void onRefresh(RefreshLayout refreshlayout);
+}
